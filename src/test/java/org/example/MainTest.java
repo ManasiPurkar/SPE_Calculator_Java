cfc5d974c865a9f54c846643e4e0f3ca3a3b6dfe
ManasiPurkar <141387487+ManasiPurkar@users.noreply.github.com>
2024-03-02 12:00:43 +0530

    Update MainTest.java

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -19,13 +19,13 @@ class MainTest {
         assertEquals(120, result);
     }
 
- /*   @org.junit.jupiter.api.Test
+   @org.junit.jupiter.api.Test
     public void testNaturalLogarithmFunction() {
         double result = Main.naturalLogarithm(2.71828);
         assertEquals(1, result, 0.0001);
     }
 
-    @org.junit.jupiter.api.Test
+  /*  @org.junit.jupiter.api.Test
     public void testPowerFunction() {
         double result = Main.power(2, 3);
         assertEquals(8, result, 0.0001);
